77d1d92f76b699c91e0e3851643c9b5541e61901
pobi <pobittrachandraroy2001@gmail.com>
2024-05-05 14:17:05 +0600

    with another example i again exlore about abstract using class

--- src/abstract-in-oop.ts
+++ src/abstract-in-oop.ts
@@ -9,19 +9,44 @@
   }
 
   //in class if i want to use interface data type so i we need impliments interface. also we can add extra method
-  class Car1 implements Vehicle {
+  class Minibus implements Vehicle {
     startEngine(): void {
-      console.log("i am starting the car");
+      console.log("Minibus: i am starting the car");
     }
     stopEngine(): void {
-      console.log("i am stoping the car");
+      console.log("Minibus: i am stoping the car");
     }
   }
 
   //creat instance
-  const instanceOfCar1 = new Car1();
+  const instanceOfMinibus = new Minibus();
 
   //use instance
-  instanceOfCar1.startEngine();
-  instanceOfCar1.stopEngine();
+  instanceOfMinibus.startEngine();
+  instanceOfMinibus.stopEngine();
+
+  //another example with direct abstract
+
+  //create abstract using class
+  abstract class Car2 {
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+  }
+
+  //extends Car2 causes abstract time we can't directly method implementation. that why need extra class.
+  class Marcedez extends Car2 {
+    startEngine(): void {
+      console.log("Marcedez: i am starting the car");
+    }
+    stopEngine(): void {
+      console.log("Marcedez: i am stoping the car");
+    }
+  }
+
+  //create instance
+  const marcedezInstance = new Marcedez();
+
+  //get instance
+  marcedezInstance.startEngine();
+  marcedezInstance.stopEngine();
 }
